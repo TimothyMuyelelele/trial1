ed214b3525a85b8d25d715a7bf28b6768e0a3934
TimothyMuyelelele <timothymuyelele@gmail.com>
2023-12-29 18:03:40 +0300

    now

---
+++ script2.js
@@ -0,0 +1,125 @@
+function toggleText() {
+    var hiddenText = document.getElementById("hiddenText");
+    if (hiddenText.style.display === "none") {
+        hiddenText.style.display = "block";
+    } else {
+        hiddenText.style.display = "none";
+    }
+}
+function badDay() {
+    var badDay = document.getElementById("badDay");
+    if (badDay.style.display === "none") {
+        badDay.style.display = "block";
+    } else {
+        badDay.style.display = "none";
+    }
+}
+function worried() {
+    var worried = document.getElementById("worried");
+    if (worried.style.display === "none") {
+        worried.style.display = "block";
+    } else {
+        worried.style.display = "none";
+    }
+}
+function unappreciated() {
+    var  unappreciated= document.getElementById("unappreciated");
+    if (unappreciated.style.display === "none") {
+        unappreciated.style.display = "block";
+    } else {
+        unappreciated.style.display = "none";
+    }
+}
+function giveup() {
+    var  giveup= document.getElementById("giveup");
+    if (giveup.style.display === "none") {
+        giveup.style.display = "block";
+    } else {
+        giveup.style.display = "none";
+    }
+}    
+function insecure() {
+    var  insecure= document.getElementById("insecure");
+    if (insecure.style.display === "none") {
+        insecure.style.display = "block";
+    } else {
+        insecure.style.display = "none";}
+    }
+ function lonely() {
+        var lonely  = document.getElementById("lonely");
+        if (lonely.style.display === "none") {
+            lonely.style.display = "block";
+        } else {
+            lonely.style.display = "none";
+        }
+}
+function sleep() {
+    var sleep = document.getElementById("sleep");
+    if (sleep.style.display === "none") {
+        sleep.style.display = "block";
+    } else {
+        sleep.style.display = "none";}
+    }
+ function Happy() {
+        var  Happy= document.getElementById("Happy");
+        if (Happy.style.display === "none") {
+            Happy.style.display = "block";
+        } else {
+            Happy.style.display = "none";}
+        }
+function mean() {
+        var  mean= document.getElementById("mean");
+        if (mean.style.display === "none") {
+           mean.style.display = "block";
+        } else {
+        mean.style.display = "none";}
+  } 
+function talk() {
+    var talk = document.getElementById("talk");
+    if (talk.style.display === "none") {
+        talk.style.display = "block";
+    } else {
+        talk.style.display = "none";}
+  }  
+function sad() {
+  var  sad= document.getElementById("sad");
+  if (sad.style.display === "none") {
+    sad.style.display = "block";
+  } else {
+    sad.style.display = "none";}
+  }  
+function argue () {
+  var argue = document.getElementById("argue");
+  if (argue.style.display === "none") {
+      argue.style.display = "block";
+  } else {
+      argue.style.display = "none";}
+  }
+function crying() {
+  var crying = document.getElementById("crying");
+  if (crying.style.display === "none") {
+    crying.style.display = "block";
+  } else {
+    crying.style.display = "none";}
+  }   
+function bored () {
+  var bored = document.getElementById("bored");
+  if (bored.style.display === "none") {
+    bored.style.display = "block";
+  } else {
+    bored.style.display = "none";}
+  }
+function  sleepy() {
+    var sleepy = document.getElementById("sleepy");
+    if (sleepy.style.display === "none") {
+        sleepy.style.display = "block";
+    } else {
+        sleepy.style.display = "none";}
+}
+function birthday() {
+    var  birthday= document.getElementById("birthday");
+    if (birthday.style.display === "none") {
+        birthday.style.display = "block";
+    } else {
+        birthday.style.display = "none";}
+    }
